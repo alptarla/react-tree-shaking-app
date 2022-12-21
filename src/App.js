274d43b546d869a9560ce36f339b967bdc175368
Alp Tarla <alptarla@gmail.com>
2022-12-21 20:03:17 +0300

    Prepare svg items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+import { ReactComponent as TreeSVG } from './assets/cartoon-tree.svg'
+import { ReactComponent as AppleSVG } from './assets/fresh-apple-icon.svg'
+import { ReactComponent as AppleBasketSVG } from './assets/garden-basket.svg'
+
 export default function App() {
-  return <div>Protel Code Challenge</div>
+  return (
+    <div>
+      <TreeSVG
+        width={600}
+        height={600}
+      />
+      <AppleSVG
+        width={50}
+        height={50}
+      />
+      <AppleBasketSVG
+        width={200}
+        height={200}
+      />
+    </div>
+  )
 }
